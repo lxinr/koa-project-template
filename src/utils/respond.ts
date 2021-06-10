4f4d4f628353f81e2c82a60bd473bcc5aa14aa78
liux <liux@imlaidian.com>
2021-06-10 22:47:47 +0800

    refactor(RespondInfo): 修改写法

--- src/utils/respond.ts
+++ src/utils/respond.ts
@@ -1,7 +1,7 @@
 interface RespondInfo {
-  result?: number;
+  result: number;
   data?: any;
-  message?: string;
+  message: string;
 }
 
 /**
@@ -13,15 +13,15 @@ interface RespondInfo {
  * @class Respond
  */
 export default class Respond {
-  public static info(message = '', result = 2, data = null) {
-    return { message, result, data } as RespondInfo
+  public static info(message = '', result = 2, data = null): RespondInfo {
+    return { message, result, data }
   }
 
-  public static suc(data: any = null, message = '请求成功') {
-    return { message, result: 1, data } as RespondInfo
+  public static suc(data: any = null, message = '请求成功'): RespondInfo {
+    return { message, result: 1, data }
   }
 
-  public static err(message = '请求失败', result = 0, data = null) {
-    return { message, result, data } as RespondInfo
+  public static err(message = '请求失败', result = 0, data = null): RespondInfo {
+    return { message, result, data }
   }
 }
